de8c5b34cc385bcba81abef7a922b2c3c1404c00
johannes <jmuljana@gmail.com>
2022-03-03 22:35:10 +1100

    database connection

--- www/index.php
+++ www/index.php
@@ -4,4 +4,16 @@ echo "hello world";
 $myvar = 20;
 $myname = "Johannes";
 echo $myname . " " . $myvar;
+$user = "root";
+$pass = "password";
+$name = "db_data";
+$host = "db";
+$connection = mysqli_connect( $host ,$user,$pass,$name, 3306);
+if( $connection ) {
+  echo "success!";
+}
+else {
+  echo "dammmit!";
+}
+
 ?>
--- www/src/Database.php
+++ www/src/Database.php
@@ -1,17 +1,51 @@
 <?php
 namespace textreview;
 
-class Database {
-  private $username;
-  private $password;
-  private $host;
-  private $db;
-  // function to set up the class
-  protected function __construct() {
+use Exception;
+use mysqli;
 
-  }
-  protected function getConnection() {
+class Database
+{
+    private $username;
+    private $password;
+    private $host;
+    private $db;
+    private $port;
+    private $db_connection;
+    // function to set up the class (refer to magic methods in php)
+    // https://www.php.net/manual/en/language.oop5.magic.php
 
-  }
+    protected function __construct()
+    {
+        // populate the credentials from variables set using SetEnv in the .htaccess file
+        $this -> username = getenv("dbuser");
+        $this -> password = getenv("dbpass");
+        $this -> host = getenv("dbhost");
+        $this -> db = getenv("dbname");
+        $this -> port = getenv("dbport");
+    }
+    private function connect()
+    {
+      try {
+        if(!$this -> username || !$this -> password || !$this -> host || !$this -> db )
+        {
+          throw new Exception("missing a db credential");
+        }
+        $this -> connection = mysqli_connect(
+          $this -> host,
+          $this -> username,
+          $this -> password,
+          $this -> db,
+          $this -> port
+        );
+      }
+      catch( Exception $exc )
+      {
+        error_log("database error: " . $exc -> getMessage() );
+      }
+    }
+    protected function getConnection()
+    {
+        return $this->db_connection;
+    }
 }
-?>
