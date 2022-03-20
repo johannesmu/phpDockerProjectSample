34384129b2fadcc5d527237dfd6006e6bcd4ba6c
johannes <jmuljana@gmail.com>
2022-03-20 19:25:47 +1100

    adding book detail page

--- www/src/Book.php
+++ www/src/Book.php
@@ -69,7 +69,52 @@ class Book extends Database {
     }
   }
 
-  public function getDetail() {
-    
+  public function getDetail( $book_id ) {
+    $query = "
+    SELECT 
+      book.book_id,
+      book_title,
+      tagline,
+      isbn13,
+      year,
+      pages,
+      image.file_name AS picture,
+      author.first AS suthor_first,
+      author.last AS author_last,
+      publisher.name AS publisher
+      FROM book 
+      INNER JOIN book_image
+      ON book.book_id = book_image.book_id
+      INNER JOIN image
+      ON book_image.image_id = image.image_id
+      INNER JOIN book_author
+      ON book.book_id = book_author.book_id
+      INNER JOIN author
+      ON book_author.author_id = author.author_id
+      INNER JOIN book_publisher
+      ON book.book_id = book_publisher.book_id
+      INNER JOIN publisher
+      ON book_publisher.publisher_id = publisher.publisher_id
+      WHERE book.book_id = ?
+    ";
+    try {
+      $statement = $this -> dbconnection -> prepare( $query );
+      if( !$statement ) {
+        throw new Exception("query error");
+      }
+      $statement -> bind_param( "i", $book_id );
+      if( !$statement -> execute() ) {
+        throw new Exception("query error");
+      }
+      else {
+        $result = $statement -> get_result();
+        $detail = $result -> fetch_assoc();
+        return $detail;
+      }
+    }
+    catch( Exception $exc) {
+      echo $exc -> getMessage();
+      return false;
+    }
   }
 }
