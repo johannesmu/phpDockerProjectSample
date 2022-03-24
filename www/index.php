858877b9cc0f08b463e53e91af336218ad12caef
johannes <jmuljana@gmail.com>
2022-03-25 09:39:20 +1100

    moved code from test.php to index

--- www/index.php
+++ www/index.php
@@ -1,21 +1,15 @@
 <?php
-// array
-// Indexed array
-$items = array();
-array_push($items, "apples");
-array_push($items, "oranges");
-print_r($items);
-echo "<br>";
-// Associative array
-$car = array();
-$car['make'] = 'Toyota';
-$car['model'] = 'Camry';
-$car['year'] = '2021';
-print_r($car);
-echo "<br>";
-// First item is Apples
-echo "First item is " . $items[0];
-echo "<br>";
-// The car make is Toyota
-echo "The car make is " . $car["make"];
+// enable composer autoloading
+require("vendor/autoload.php");
+
+//request classes from autoloader
+use textreview\Book;
+
+$book = new Book();
+$items = $book -> getItems();
+
+$loader = new \Twig\Loader\FilesystemLoader("templates");
+$twig = new Twig\Environment( $loader, [ "cache" => false ] );
+
+echo $twig -> render("home.html.twig", ["page_title" => "TextReview", "greeting" => "Hello there", "books" => $items ] );
 ?>
