2e8a2c2fe8fc40b0c9251a247f09e24650ba5ebf
johannes <jmuljana@gmail.com>
2022-04-01 10:03:42 +1100

    search functionality

--- www/src/Search.php
+++ www/src/Search.php
@@ -17,7 +17,8 @@ class Search extends Database {
     FROM book 
     WHERE book_title LIKE ?";
     $statement = $this -> dbconnection -> prepare( $query );
-    $statement -> bind_param("s", "%$term%" );
+    $search_term = "%$term%";
+    $statement -> bind_param("s", $search_term );
     try {
       if( !$statement -> execute() ) {
         throw new Exception("query error");
