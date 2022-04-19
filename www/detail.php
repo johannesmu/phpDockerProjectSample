0a14d38f69ef08ae49970165133d6d012d881c21
johannes <jmuljana@gmail.com>
2022-04-19 19:12:49 +1000

    added session data

--- www/detail.php
+++ www/detail.php
@@ -3,6 +3,7 @@
 require("vendor/autoload.php");
 
 use textreview\Book;
+use textreview\Session;
 
 // global variable
 $book_id = $_GET['id'];
@@ -12,6 +13,8 @@ $book = new Book();
 $detail = $book -> getDetail($book_id);
 $book_title = $detail['book_title'];
 $site_name = "Textreview";
+$email = Session::get("email");
+$account_id = Session::get("account_id");
 
 $loader = new \Twig\Loader\FilesystemLoader("templates");
 $twig = new Twig\Environment( $loader, [ "cache" => false ] );
@@ -19,10 +22,10 @@ $twig = new Twig\Environment( $loader, [ "cache" => false ] );
 echo $twig -> render("detail.html.twig", [
   "page_title" => "TextReview $book_title",
   "book" => $detail,
-  "site_name" => $site_name
-  ] );
-// javascript car = { "colour" : "yellow", "brand" : "Honda" }
-// c = car.colour
-$car = array("colour" => "yellow", "brand" => "Honda");
-$c = $car["colour"];
+  "site_name" => $site_name,
+  "email" => $email,
+  "account_id" => $account_id
+  ] 
+);
+
 ?>
