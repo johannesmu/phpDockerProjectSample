107caa91e30f9ba1b33ea7eb099c5784b4594851
johannes <jmuljana@gmail.com>
2022-04-19 17:01:22 +1000

    New classes for Review and Favourite

---
+++ www/src/Favourite.php
@@ -0,0 +1,10 @@
+<?php
+namespace textreview;
+
+use textreview\Database;
+use Exception;
+
+class Favorite extends Database {
+  
+}
+?>
---
+++ www/src/Review.php
@@ -0,0 +1,10 @@
+<?php
+namespace textreview;
+
+use textreview\Database;
+use Exception;
+
+class Review extends Database {
+  
+}
+?>
