869d5bb8f3e8f3b5fc7f06df6d3eb8dce2e6f9f6
johannes <jmuljana@gmail.com>
2022-04-19 19:45:29 +1000

    adding review form

--- www/detail.php
+++ www/detail.php
@@ -4,6 +4,23 @@ require("vendor/autoload.php");
 
 use textreview\Book;
 use textreview\Session;
+use textreview\Review;
+
+if( $_SERVER["REQUEST_METHOD"] == "POST" ) {
+  $review = new Review();
+  $book_id = $_POST["book_id"];
+  $account_id = $_POST["account_id"];
+  $title = $_POST["title"];
+  $text = $_POST["text"];
+  // create the review
+  $add = $review -> addReview($book_id, $account_id, $title, $text );
+  if( $add == true ) {
+    // tell user review has been posted
+  }
+  else {
+    // tell user review is not created
+  }
+}
 
 // global variable
 $book_id = $_GET['id'];
